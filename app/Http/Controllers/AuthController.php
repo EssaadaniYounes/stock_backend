39b5e9401b37b1166afd9748b2675e0763144844
EssaadaniYounes <younesessaadani2003@gmail.com>
2022-08-18 00:34:57 +0200

    finish login part

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,8 +38,14 @@ class AuthController extends Controller
         if (auth()->attempt($data)) {
             $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
             $role= Role::find( auth()->user()->role_id);
-
-            return response()->json(['token' => $token,'role'=>$role], 200);
+            
+            $user = auth()
+                    ->user()
+                    ->join('roles', 'roles.id', '=', 'users.role_id')
+                    ->select('users.*', 'roles.*')
+                    ->first();
+            
+            return response()->json(['token' => $token,'data'=>$user], 200);
         } else {
             return response()->json(['success'=>false,'error' => 'Unauthorised'] );
         }
